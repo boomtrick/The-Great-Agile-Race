a35da57256488a1d23c61e5b717e1f0612a7cd17
Azik-Chitown <azamat6056@gmail.com>
2017-10-10 17:03:28 -0500

    creating Position class

---
+++ src/Controllers/Position.java
@@ -0,0 +1,20 @@
+package Controllers;
+/**
+ * 
+ * created by Azamat Abdikadirov by Azamat Abdikadirov
+ * 
+ * The class creates a data structure that holds information about positions of Player and other game objects
+ *
+ */
+public class Position {
+
+	private int row;
+	private int col;
+	
+	public Position(int row_, int col_) {
+		row=row_;
+		col=col_;
+		
+	}
+	
+}
