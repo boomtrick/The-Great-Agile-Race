06c9c951d81860a972fdc5f57da7b8021b9cfa69
Joce <go.joce@gmail.com>
2017-10-12 22:43:49 -0500

    Reached file with parser.

--- src/Controllers/DialogueParser.java
+++ src/Controllers/DialogueParser.java
@@ -2,21 +2,34 @@ package Controllers;
 
 import Models.Fact;
 import com.google.gson.Gson;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+//Parses Dialogue.json
 public class DialogueParser {
 
-
-
-    Gson gson = new Gson();
-
-    Fact fact = gson.fromJson("{\"name\": \"John\"}", Fact.class);
-    //DialogueSingleton fact = gson.fromJson("{\"name\": \"John\"}", DialogueSingleton.class);
-//Code will go here to parse Json
-
-
-
-////For testing
     public static void main(String[] args)
     {
-        System.out.println("test");
+
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("src/Config/Dialogue.json")) {
+            // Convert JSON to Java Object
+            Fact fact = gson.fromJson(reader, Fact.class);
+            System.out.println(fact.toString());
+
+
+            //List<Fact> facts = fact.getFact();
+
+            // Convert JSON to JsonElement, and later to String
+            /*JsonElement json = gson.fromJson(reader, JsonElement.class);
+            String jsonInString = gson.toJson(json);
+            System.out.println(jsonInString);*/
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
