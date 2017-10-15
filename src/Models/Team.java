9cadc77a7442bdd66abb76dd6722e48d8058c6bf
Joce <go.joce@gmail.com>
2017-10-15 13:48:31 -0500

    Created team class, made BoardConstants final

---
+++ src/Models/Team.java
@@ -0,0 +1,28 @@
+package Models;
+
+
+public class Team {
+
+    /*
+    Holds getters, setters and methods for accessing Fact data
+ */
+    private String color;
+
+    //private List<Player> players;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+//    public List<Player> getPlayers() {
+//        return players;
+//    }
+//
+//    public void setPlayers(List<Player> players) {
+//        this.players = players;
+//    }
+}
