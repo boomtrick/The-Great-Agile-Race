fa714358b70a3f85121fd36fbc611fe320b9e898
Zalman Gagerman <ziggibyte@gmail.com>
2017-10-15 00:19:58 -0500

    Some work done on Tile and Board

--- src/Controllers/Board.java
+++ src/Controllers/Board.java
@@ -2,18 +2,31 @@ package Controllers;
 /**
  * Created by zalmangagerman on 10/8/17.
  */
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Board implements BoardI {
 
-    private final List<List<PlayerI>> teams_;
+    private final List<PlayerI> players_;
     private final List<TileI> tiles_;
     private final int rowSize_;
+    private final int colSize_;
 
 
-    public Board(int rowSize, List<List<PlayerI>> teams) {
+    public Board(int rowSize, int colSize, List<PlayerI> teams) {
         rowSize_ = rowSize;
-        teams_ = teams;
+        colSize_ = colSize;
+        tiles_ = new ArrayList<>();
+        players_ = teams;
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < rowSize; j++) {
+                if (i == 0 & j == 0) {
+                    tiles_.add(new Tile(i, j, players_));
+                }
+                tiles_.add(new Tile(i, j));
+            }
+        }
     }
 
     @Override
--- src/Controllers/Tile.java
+++ src/Controllers/Tile.java
@@ -1,7 +1,59 @@
 package Controllers;
-
 /**
  * Created by zalmangagerman on 10/8/17.
  */
+import static Config.BoardConstants.Team.*;
+
+import java.util.ArrayList;
+import java.util.List;
 public class Tile implements TileI {
+    private final int row_;
+    private final int col_;
+    private List<PlayerI> players_;
+
+    public Tile(int row, int col) {
+        players_ = new ArrayList<>();
+        row_ = row;
+        col_ = col;
+    }
+
+
+    public Tile(int row, int col, List<PlayerI> players) {
+        row_ = row;
+        col_ = col;
+        players_ = players;
+    }
+
+    @Override
+    public int getRow() {
+        return row_;
+    }
+
+    @Override
+    public int getCol()
+    {
+        return col_;
+    }
+
+    @Override
+    public void insertPlayer(PlayerI newPlayer) {
+        if (newPlayer.getTeam() == red) {
+            players_.add(newPlayer);
+        } //TOdo streamline
+        System.out.println(newPlayer.getId() + " added to tile <" + row_ + ", " + col_ + ">");
+    }
+
+    @Override
+    public void deletePlayerById(String id) {
+        int origSize = players_.size();
+        for (PlayerI player : players_) {
+            if (player.getId().equals(id)) {
+                players_.remove(player);
+                System.out.println(id + " deleted from tile <" + row_ + ", " + col_ + ">");
+            }
+        }
+        if (players_.size() == origSize) {
+            throw new IllegalArgumentException(id + " wasn't on the tile");
+        }
+    }
 }
