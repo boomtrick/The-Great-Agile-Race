d1deea397f35c7ad86a5b3f121bc262ce913b037
Azik-Chitown <azamat6056@gmail.com>
2017-10-22 00:24:13 -0500

    Replaced old Team class with Player class to avoid confusion

---
+++ src/Controllers/Player.java
@@ -0,0 +1,14 @@
+package src.Controllers;
+
+import java.awt.Image;
+
+import src.Config.BoardConstants.Team;
+
+public class Player extends PlayerAbsract {
+
+	public Player(String name, Image image, int id, Team team) {
+		super(name, image, id, team);
+		// TODO Auto-generated constructor stub
+	}
+
+}
