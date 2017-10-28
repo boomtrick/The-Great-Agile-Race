dfb428dc186cf7442d0e9e8bfa936422bb88d4f0
Joce <go.joce@gmail.com>
2017-10-28 14:37:50 -0500

    Changed Image datatypes to BufferedImage

--- src/Controllers/Player.java
+++ src/Controllers/Player.java
@@ -2,11 +2,11 @@ package Controllers;
 
 import Config.BoardConstants.TeamColor;
 
-import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends PlayerAbsract {
 
-	public Player(String name, Image image, int id, TeamColor team) {
+	public Player(String name, BufferedImage image, int id, TeamColor team) {
 		super(name, image, id, team);
 		// TODO Auto-generated constructor stub
 	}
